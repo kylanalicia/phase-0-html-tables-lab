08750a5c5d5846f61f2dff68796cae5740e4c7a4
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-07-21 12:56:30 +0300

    completed assignment

---
+++ bank/flatiron/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="ui raised segment">
+            <div className="ui segment violet inverted">
+                <h1>The Bank of Flatiron</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App;
